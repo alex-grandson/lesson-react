d4bc8ec80e6bbf25a88b6f091b064e52c953b32a
= <=>
2022-11-27 14:07:14 +0300

    feat: add todoitems from server; delete button

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, ButtonGroup, Flex } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { TodoApi } from "../api/todoApi"
 import TodoItem from "./TodoItem"
@@ -9,26 +10,57 @@ const TodoList = () => {
         TodoApi.getTodos()
             .then((response) => {
                 console.log(response.data)
-                setTodos(response.data)
+                setTodos(response.data.map((item) => ({
+                    ...item,
+                    isDeleted: false
+                })))
             })
             .catch((err) => {
                 console.log(err);
             })
     }
 
+    const deleteItem = (i: number) => {
+        console.log('deleting ', i);
+        
+        setTodos(todos.map((item, idx) => ({
+            ...item,
+            isDeleted: idx == i ? true : item.isDeleted
+        })))
+    }
+
+    const changeAll = (newState: boolean) => {
+        console.log(todos);
+        
+        setTodos(todos.map((item) => ({
+            ...item,
+            completed: newState
+        })))
+    }
+
     useEffect(() => {
-        loadData()
+        if (todos.length == 0) {
+            loadData()
+        }
     }, [])
 
     return (
-        <div>
-            <TodoItem serial={1} todo={{
-                userId: 1,
-                id: 1,
-                title: "delectus aut autem",    
-                completed: true
-            }} />
-        </div>
+        <Flex flexDirection={'column'} gap={5}>
+            <ButtonGroup>
+                <Button onClick={() => changeAll(false)}>
+                    Uncheck all
+                </Button>
+                <Button onClick={() => changeAll(true)}>
+                    Check all
+                </Button>
+            </ButtonGroup>
+            {
+                todos.map((item, idx) => {
+                    return <TodoItem todo={item} serial={idx + 1} key={idx} 
+                    deleteItem={() => deleteItem(idx)} />
+                })
+            }
+        </Flex>
     )
 }
 
